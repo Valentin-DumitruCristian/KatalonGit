eea127bfc1522461797ed9eb8973cf2e57452e9f
Valibaws <Valibaws@DESKTOP-9RN9EQO>
2024-11-16 19:38:13 +0200

    Created a test suite

---
+++ Test Suites/Purchase Flow Desktop.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Purchase Flow Desktop</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>8f07690b-e157-4eb5-bfc8-1dcf0406ea3b</testSuiteGuid>
+   <testCaseLink>
+      <guid>66a03c60-472b-4c7c-9da6-b33f9a043ecf</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Purchase Flow/PDPtoCheckout-DESKTOP</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
